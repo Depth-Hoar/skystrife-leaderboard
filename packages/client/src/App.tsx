49fbd44eace961cdf121e511da4316157d3b5616
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-11-27 17:49:34 -0700

    killCount-testing

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,5 @@
-import { useEntityQuery } from "@latticexyz/react";
-import { Has, getComponentValueStrict } from "@latticexyz/recs";
+import { useEntityQuery, useComponentValue } from "@latticexyz/react";
+import { Has, HasValue, getComponentValueStrict, runQuery, getComponentValue, Not } from "@latticexyz/recs";
 import { toEthAddress } from "@latticexyz/utils";
 import { decodeEntity } from "@latticexyz/store-sync/recs";
 import { useMUD } from "./MUDContext";
@@ -11,9 +11,115 @@ const TOKEN_ID =
 
 export const App = () => {
   const {
-    components: { TokenBalance },
+    components: { 
+      tables,
+      TokenBalance, 
+      Combat, 
+      CombatResult,
+      MatchConfig, 
+      Match, 
+      KillCount, 
+      UnitType, 
+      MatchReward,
+      MatchReady,
+      Position,
+      TerrainType,
+      MatchFinished
+    },
   } = useMUD();
 
+
+  
+  // console.log(Combat.schema,'combat');
+  console.log(tables,'tables');
+  // console.log(Match,'Match');
+  //runQuery([Has(MatchConfig), HasValue(Match, { value: 4 })])
+  // console.log(TokenBalance.metadata.keySchema,"keySchema")
+
+  const matchId = 310
+  const matchIdEntity = useEntityQuery([Has(MatchConfig), HasValue(Match, { value: matchId  })])
+  // console.log(matchIdEntity,"match entity")
+  const config = useComponentValue(MatchConfig, matchIdEntity);
+  const rewards = useComponentValue(MatchReward, matchIdEntity);
+  // console.log(config,"config")
+  // console.log(rewards,"rewards")
+  // const matchNumber = config.matchNumber;
+  // console.log(KillCount,"kill count")
+  // console.log(MatchConfig,"MatchConfig")
+  // console.log(UnitType,"UnitType")
+  const getUnitType = UnitType.metadata.keySchema;
+  // const getUnitTypes = useEntityQuery([Has(matchNumber),HasValue(UnitType, { value: 100  })])
+  
+  // console.log(getUnitType,"get unit type")
+
+
+
+  const entitiesInMatch = [...runQuery([HasValue(Match, { matchId: matchId })])];
+
+  console.log(entitiesInMatch,"entities in match")
+  
+  entitiesInMatch.forEach(entity => {
+    const killCount = getComponentValue(KillCount, entity);
+    if (!killCount) {
+      console.log(`Entity ${entity} has no kills`);
+      return;
+    }  
+  
+    console.log(`Entity ${entity} has ${killCount.value} kills`);
+  });
+
+
+
+
+
+  // const test = [...runQuery([HasValue(Position, { z: matchEntity })])].forEach(entity => {
+  //   const killCount = getComponentValue(KillCount, entity);
+  //   if (!killCount) {
+  //     console.log(`Entity ${entity} has no kills`);
+  //   }  
+  
+  //   console.log(`Entity ${entity} has ${killCount.value} kills`);
+  // });
+
+  // ---------------------This code works don't loose it----------------------------------------------------------------
+  // const test = [...runQuery([HasValue(Position, { z: matchId}, Not(TerrainType))])]
+  // console.log(test,"test");
+
+
+
+  function logKillCounts() {
+    const matchEntity = 10; // match number
+  
+    [...runQuery([HasValue(Position, { z: matchEntity })])].forEach(entity => {
+      const killCount = getComponentValue(KillCount, entity);
+      if (!killCount) {
+        console.log(`Entity ${entity} has no kills`);
+        return;
+      }  
+  
+      console.log(`Entity ${entity} has ${killCount.value} kills`);
+    });
+  }
+
+  // function Capturable() {
+  //   const matchEntity = 310;
+  
+  //   [...runQuery([HasValue(Capturable, { z: matchEntity })])].forEach(entity => {
+  //     const killCount = getComponentValue(KillCount, entity);
+  //     if (!killCount) {
+  //       console.log(`Entity ${entity} has no kills`);
+  //       return;
+  //     }  
+  
+  //     console.log(`Entity ${entity} has ${killCount.value} kills`);
+  //   });
+  // }
+  
+  logKillCounts();
+
+
+
+  
   const balances = useEntityQuery([Has(TokenBalance)])
     .filter((entity) => {
       return (
